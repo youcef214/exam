15943e557224b9910250d03ae18676145df010a1
youcef214 <87040553+youcef214@users.noreply.github.com>
2021-07-06 22:30:09 +0200

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -6,7 +6,7 @@ from os import environ
 from imperios import app
 
 if __name__ == '__main__':
-    HOST = '0.0.0.0'
+    HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '5555'))
     except ValueError:
